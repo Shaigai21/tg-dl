ec7a42264fcaac2aee58dd535176e1ed6b1649f8
Mastah <blackmastah21@gmail.com>
2025-06-08 21:08:39 +0300

    Implement downloader function
    Needed for further use; downloads a video via URL and returns filename and title.
    
    Signed-off-by: Mastah <blackmastah21@gmail.com>

---
+++ youtube_downloader/__init__.py
@@ -0,0 +1,30 @@
+import yt_dlp
+import os
+
+
+def download_video(url):
+    if not url.startswith("https://www.youtube.com") and not url.startswith("https://youtube.com"):
+        raise ValueError("Not youtube link")
+    try:
+        # Папка для загрузки
+        download_folder = "tg_dl_downloads"
+        if not os.path.exists(download_folder):
+            os.makedirs(download_folder)
+
+        # Настройки для yt-dlp
+        ydl_opts = {
+            'format': 'bestvideo+bestaudio/best',
+            'retries': 20,
+            'outtmpl': f'{download_folder}/%(title)s.%(ext)s',
+            'noplaylist': True,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            video_title = info_dict.get('title', 'video')
+            video_filename = ydl.prepare_filename(info_dict)
+
+    except Exception as e:
+        raise NameError("Download error")
+
+    return video_filename, video_title
