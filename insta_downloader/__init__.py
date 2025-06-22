1529953f7aa93667bbd67234f0c1ad995c6ddafd
PovarovAA <povarovan2004@gmail.com>
2025-06-22 17:28:55 +0300

    Clean up temporary files (JPG/JSON/TXT) after download added

--- insta_downloader/__init__.py
+++ insta_downloader/__init__.py
@@ -1,6 +1,7 @@
 import os
 import instaloader
 
+
 def download_video(url):
     if not url.startswith("https://www.instagram.com/reel/"):
         raise ValueError("Not an Instagram link")
@@ -12,6 +13,11 @@ def download_video(url):
         if not os.path.exists(download_folder):
             os.makedirs(download_folder)
         loader.download_post(post, target=download_folder)
+        # remove jpg and json files from download_folder
+        for file in os.listdir(download_folder):
+            if file.endswith(".jpg") or file.endswith(".json.xz") or file.endswith(".txt"):
+                os.remove(f"{download_folder}/{file}")
+
         video_filename = f"{download_folder}/{post_id}.mp4"
         return video_filename, post.title
     except Exception as e:
